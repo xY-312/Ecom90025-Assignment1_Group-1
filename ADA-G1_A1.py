bdf84ffe61cfd94839485305bfffcdeace54d64e
xY-312 <n8b749dccr@privaterelay.appleid.com>
2026-08-09 22:05:36 +1000

    Integrate Kaggle API for data download and submission
    Added functionality to download competition data from Kaggle and submit predictions.

--- ADA-G1_A1.py
+++ ADA-G1_A1.py
@@ -1,6 +1,30 @@
-
+import os
+import zipfile
+from kaggle.api.kaggle_api_extended import KaggleApi  
 import pandas as pd
 
+api_key = "KGAT_e8f00f8e7a07bf425e34ed96e848ccf6"
+os.makedirs(os.path.expanduser("~/.kaggle"), exist_ok=True)
+
+with open(os.path.expanduser("~/.kaggle/access_token"), "w") as f:
+    f.write(api_key)
+
+api = KaggleApi()
+api.authenticate()
+
+COMPETITION_ID = "ecom-90025-2026-sm-2-ada-assignment-and-practice"
+
+DESTINATION_PATH = "./data"
+api.competition_download_files(competition=COMPETITION_ID, path=DESTINATION_PATH)
+
+zip_filepath = os.path.join(DESTINATION_PATH, f"{COMPETITION_ID}.zip")
+with zipfile.ZipFile(zip_filepath, 'r') as zip_ref:
+    zip_ref.extractall(DESTINATION_PATH)
+os.remove(zip_filepath)
+
+print(f"Files extracted successfully to '{DESTINATION_PATH}' directory.")
+
+# Read data
 train = pd.read_csv("train_data.csv")
 test = pd.read_csv("test_data.csv")
 
@@ -51,6 +75,22 @@ print("R square =", model.score(train[['X34']], train['Y']))
 
 # predict Y with the estimated betas
 test_predictions = model.predict(test[['X34']])
+
+
+# save the predictions to a CSV file
+submission = pd.read_csv("submission.csv")
+submission["Y"] = test_predictions
+submission.to_csv("submission_final.csv", index=False)
+
+api.competition_submit(
+    file_name="./submission_final.csv",
+    message="submission_Assign1",
+    competition=COMPETITION_ID
+)
+
+subs = api.competition_submissions(COMPETITION_ID)
+for s in subs:
+    print(vars(s))
 
 
 # save the predictions to a CSV file
